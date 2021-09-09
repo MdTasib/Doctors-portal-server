02ba81084c323d1690fb5e08458ba6959ebe656e
MdTasib <ohidulalam442@gmail.com>
2021-09-09 12:10:26 +0600

    database connect

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello');
+  res.send('Hello World');
 });
+
+
+const uri = `mongodb+srv://doctorsPortal:doctorsPortal@cluster0.do24a.mongodb.net/Appointments?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("Appointments").collection("bookings");
+  
+});
+
+
 
 app.listen(5000);
